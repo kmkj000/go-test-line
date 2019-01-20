7f1a8387a9e9cad6d85a240c91fd08588e26e786
kmkj000 <kd1191681@st.kobedenshi.ac.jp>
2019-01-20 17:23:26 +0900

    🍸 use Gin and echo bot --notest

---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.POST("/callback", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return
+		}
+
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+
+	router.Run(":" + port)
+
+}
